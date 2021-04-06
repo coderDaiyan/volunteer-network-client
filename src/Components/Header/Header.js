788a9deed7a595ae96a1fd54d79d273b11a68ced
coderDaiyan <daiyanabdallah@gmail.com>
2021-04-06 12:37:16 +0600

    header done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand nav-logo" to="">
+              <img src="https://i.ibb.co/jhkvYqd/logo.png" alt="" />
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <span className="me-auto"></span>
+              <div class="d-flex">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="/"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login">
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/events">
+                      Your Events
+                    </Link>
+                  </li>
+                  <Link to="/admin">
+                    <button class="btn btn-outline-danger" type="button">
+                      Admin
+                    </button>
+                  </Link>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Header;
